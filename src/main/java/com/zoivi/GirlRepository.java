9e9f0b5c9c80839c7999e5e64b70378a3890a558
qiuhuanran <qiuhuanran@qq.com>
2017-03-27 00:02:41 +0800

    Initial commit

---
+++ src/main/java/com/zoivi/GirlRepository.java
@@ -0,0 +1,9 @@
+package com.zoivi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Administrator on 2017/3/26 0026.
+ */
+public interface GirlRepository extends JpaRepository<Girl, Integer>{
+}
